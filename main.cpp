70f3e0cc7ecf1d4b7d750bb5c919ecb8b30ba1c0
Borna <bornabarahimi@gmail.com>
2019-02-27 12:12:33 +0330

    add substraction

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 #include<string>
 
 
-int main(){
+int main() {
 	std::string input;
 	std::getline(std::cin, input);
 	input.push_back(' ');
 
-	std::vector<std::string> elements;
+	std::vector <std::string> elements;
 
 	std::string temp;
-	for(std::string::iterator it = input.begin(); it != input.end(); ++it) {
+	for (std::string::iterator it = input.begin(); it != input.end(); ++it) {
 		if (not isspace(*it)) {
 			temp.push_back(*it);
 		} else {
@@ -24,29 +24,39 @@ int main(){
 
 	int result = 0;
 	bool wait_for_number = true, error = false;
-	for ( std::vector<std::string>::iterator it = elements.begin(); it != elements.end(); ++it ) {
+	std::string operation_type = "+";
+	for (std::vector<std::string>::iterator it = elements.begin(); it != elements.end(); ++it) {
 		bool is_number = true;
-		for ( std::string::iterator cit = it->begin(); cit != it->end(); ++cit)
+		for (std::string::iterator cit = it->begin(); cit != it->end(); ++cit)
 			if (not isdigit(*cit))
 				is_number = false;
 
 		if (is_number) {
 			if (wait_for_number) {
-				result += std::atoi(it->c_str());
+				if (operation_type == "+") {
+					result += std::atoi(it->c_str());
+				}
+				if (operation_type == "-") {
+					result -= std::atoi(it->c_str());
+				}
 				wait_for_number = false;
 			} else {
-				std::cout << "ERROR" << std::endl;
+				std::cout << "ERROR1" << std::endl;
 				return -1;
 			}
 		} else {
 			if (wait_for_number) {
-				std::cout << "ERROR" << std::endl;
+				std::cout << "ERROR2" << std::endl;
 				return -1;
 			} else {
-				if ((*it) == "+"){
+				if ((*it) == "+") {
+					operation_type = *it;
+					wait_for_number = true;
+				} else if ((*it) == "-") {
+					operation_type = *it;
 					wait_for_number = true;
 				} else {
-					std::cout << "ERROR" << std::endl;
+					std::cout << "ERROR3" << std::endl;
 					return -1;
 				}
 			}
